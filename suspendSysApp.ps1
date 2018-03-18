b737ffc67e5bea6127746a5446ccf01d2a5aa305
schlitzws <30422028+schlitzws@users.noreply.github.com>
2018-03-18 00:47:09 -0500

    This script runs the kill method on whatever process you match.
    I created this program to help mitigate cpu read write from Cortana. I don't know about you but I don't like the idea of a program I didn't create r/w to wherever they want without my ability to monitor.

---
+++ suspendSysApp.ps1
@@ -0,0 +1,21 @@
+﻿function killcortana {ps | ? {$_.Name -match "SearchUI"}}
+$counter = $null
+$kc = killcortana
+function killingcortana {
+if($kc -and $counter -eq 0){
+echo true
+$counter += 1
+try {
+    $kc.Kill()
+} catch {
+    $kc.Dispose()
+}
+} else {
+echo false
+$counter += 0
+killingcortana 
+}
+}
+while($counter -eq $null){
+killingcortana
+}
